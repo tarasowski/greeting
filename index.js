7a63aa950c046886d73af1ba049634830016e145
Dimi Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-01-31 12:18:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const core = require('@actions/core');
 try {
   const firstName = core.getInput('first_name');
   const lastName = core.getInput('last_name');
-  console.log(`This is my firstname, ${first_name}!`);
-  console.log(`This is my lastname, ${last_name}!`);
+  console.log(`This is my firstname, ${firstName}!`);
+  console.log(`This is my lastname, ${lastName}!`);
   const date = new Date();
   console.log(`The current time is ${date.toISOString()}`);
 } catch (error) {
